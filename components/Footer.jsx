8ed003bea26dc0c2db16bb2defd52bc9b9603df3
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-03-07 12:29:41 +0500

    bugfix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="border-t border-t-gray-300 h-14 content-center w-full grid px-10 z-40 relative bg-white">
       <div className="right-0">
-        <a target="_blank" href="https://georgy-aslanyan.vercel.app/">
+        <a target="_blank" rel="noreferrer" href="https://georgy-aslanyan.vercel.app/">
           <p className="font-semibold">© 2023. by Georgy Aslanyan</p>
         </a>
       </div>
