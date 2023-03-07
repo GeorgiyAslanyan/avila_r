8366ec48f486c8e6403ec5fd6dd5c46613dd2903
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-03-07 12:18:34 +0500

    favicon commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({children}) => {
     <div>
       <Head>
         <title>AVILA</title>
-        <meta name="description" content="Avila internet shop" />
+        <meta name="description" content="Avila shop" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
